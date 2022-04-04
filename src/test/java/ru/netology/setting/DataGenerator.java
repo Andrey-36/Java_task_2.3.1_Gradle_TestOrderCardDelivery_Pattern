07ae5b80afd08a5985e53a82f7307ff29de379b1
Andrey Kolotukhin <KolotukhinAndrey@rambler.ru>
2022-04-04 22:38:06 +0300

    Added separate method for date generation

--- src/test/java/ru/netology/setting/DataGenerator.java
+++ src/test/java/ru/netology/setting/DataGenerator.java
@@ -1,19 +1,43 @@
 package ru.netology.setting;
 
 import com.github.javafaker.Faker;
-import lombok.experimental.UtilityClass;
+import lombok.Data;
+import lombok.Getter;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
-@UtilityClass
+@Data
+@Getter
 public class DataGenerator {
 
-    @UtilityClass
-    public class Registration {
-        public static RegistrationByCardInfo registrationInfo(String locale) {
-            Faker faker = new Faker(new Locale(locale));
-            return new RegistrationByCardInfo(faker.address().cityName(),
-                    faker.name().fullName(), faker.phoneNumber().phoneNumber());
-        }
+    public DataGenerator() {
+    }
+
+    private static final Faker faker = new Faker(new Locale("ru"));
+
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String getRandomCity() {
+        String city = faker.address().city();
+        return city;
+    }
+
+    public static String getRandomName() {
+        String name = faker.name().name();
+        return name;
+    }
+
+    public static String getRandomPhone() {
+        String phone = faker.phoneNumber().phoneNumber();
+        return phone;
+    }
+
+    public static RegistrationByCardInfo getUser() {
+        RegistrationByCardInfo user = new RegistrationByCardInfo(getRandomCity(), getRandomName(), getRandomPhone());
+        return user;
     }
 }
--- src/test/java/ru/netology/test/TestCardDeliveryOrder.java
+++ src/test/java/ru/netology/test/TestCardDeliveryOrder.java
@@ -2,29 +2,24 @@ package ru.netology.test;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.github.javafaker.Faker;
+import lombok.Data;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import ru.netology.setting.DataGenerator;
 import ru.netology.setting.RegistrationByCardInfo;
 
 import java.time.Duration;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
+@Data
 public class TestCardDeliveryOrder {
-    private static Faker faker;
-    RegistrationByCardInfo info = DataGenerator
-            .Registration
-            .registrationInfo("ru");
-
-    LocalDate localDate = LocalDate.now().plusDays(3);
-    DateTimeFormatter data = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    String strData = localDate.format(data);
+    private int DAY_1 = 3;
+    private int DAY_2 = 5;
+    RegistrationByCardInfo info = DataGenerator.getUser();
+    DataGenerator data = new DataGenerator();
 
     @Test
     void shouldCardDeliveryOrder() {
@@ -32,24 +27,21 @@ public class TestCardDeliveryOrder {
         open("http://localhost:9999");
         $("[data-test-id = 'city'] input").val(info.getCity());
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").val(strData);
+        $("[data-test-id='date'] input").val(data.generateDate(this.DAY_1));
         $("[data-test-id = 'name'] input").val(info.getName());
         $("[data-test-id = 'phone'] input").val(info.getPhone());
         $(".checkbox__box").click();
         $(withText("Запланировать")).click();
         $("[class = 'notification__title']").shouldHave(Condition.text("Успешно!"), Duration.ofSeconds(15));
-        $("[class = 'notification__content']").shouldHave(Condition.text("Встреча успешно запланирована на " + strData), Duration.ofSeconds(15));
-        LocalDate localDate2 = LocalDate.now().plusDays(5);
-        DateTimeFormatter data2 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        $("[class = 'notification__content']").shouldHave(Condition.text("Встреча успешно запланирована на " + data.generateDate(this.DAY_1)), Duration.ofSeconds(15));
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        String strData2 = localDate.format(data);
-        $("[data-test-id='date'] input").val(strData2);
+        $("[data-test-id='date'] input").val(data.generateDate(this.DAY_2));
         $(withText("Запланировать")).click();
         $("[data-test-id='replan-notification'] .notification__content")
                 .should(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
         $("[data-test-id='replan-notification'] .button").click();
         $("[data-test-id='success-notification'] .notification__content")
-                .should(Condition.text("Встреча успешно запланирована на " + strData2), Duration.ofSeconds(15));
+                .should(Condition.text("Встреча успешно запланирована на " + data.generateDate(this.DAY_2)), Duration.ofSeconds(15));
     }
 
     @Test
@@ -58,12 +50,12 @@ public class TestCardDeliveryOrder {
         open("http://localhost:9999");
         $("[data-test-id = 'city'] input").val(info.getCity());
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").val(strData);
+        $("[data-test-id='date'] input").val(data.generateDate(this.DAY_1));
         $("[data-test-id = 'name'] input").val("Фёдор Лебедев");
         $("[data-test-id = 'phone'] input").val(info.getPhone());
         $(".checkbox__box").click();
         $(withText("Запланировать")).click();
         $("[class = 'notification__title']").shouldHave(Condition.text("Успешно!"), Duration.ofSeconds(15));
-        $("[class = 'notification__content']").shouldHave(Condition.text("Встреча успешно запланирована на " + strData), Duration.ofSeconds(15));
-            }
+        $("[class = 'notification__content']").shouldHave(Condition.text("Встреча успешно запланирована на " + data.generateDate(this.DAY_1)), Duration.ofSeconds(15));
+    }
 }
